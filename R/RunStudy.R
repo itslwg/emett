64f6198b657bdec5ec1a52a4f94cf496d425e539
Ludde <ludviggerdin@gmail.com>
2020-07-28 15:33:34 +0200

    Drop only relevant variables

--- R/RunStudy.R
+++ R/RunStudy.R
@@ -14,45 +14,31 @@ RunStudy <- function(verbose = TRUE) {
     ## Set parameters that are default in make.study
     n.bootstrap.samples <- 2
     n.partitions <- 3
-    ## Define study name, authors, description of the study, and relevant variables
-    study.name <- "SuperLearner vs Clinicians"
-    authors <- c("Martin Gerdin Wärnberg", "Ludvig Wärnberg Gerdin")
-    description <- "A comparison of triage conducted by clinicians and the ensemble algorithm SuperLearner."
     study.variables <- c("age", "moi", "sex", "mot", "tran", "s30d", "egcs", "mgcs", "vgcs", "avpu", "hr",
                             "sbp", "dbp", "spo2", "rr", "tc", "ic", "doar", "toar", "doi", "toi", "s24h", "hd",
                             "taicu", "daicu", "tos", "dos", "nomesco", "snomed", "s", "iss")
-    pretty.names <- c("SuperLearner", "Random Forest")
+    pretty.model.names <- c("SuperLearner", "Random Forest")
+    ## Fetch and merge datasets and conduct feature engineering
     study.sample <- PrepareSample(
         study.variables=study.variables
     )
+    ## Run modelling on the outcomes of interest
     settings <- list(
         s30d=list(
             outcome.label="s30d",
             variables.to.drop=c(
-                "nomesco",
-                "snomed",
-                "s",
-                "iss",
-                "taicu",
-                "daicu",
+                "s24h",
                 "composite"
             )
         )
-        # composite=list(
-        #     outcome.label="composite",
-        #     variables.to.drop=c("s30d")
-        # )
     )
-    ## Run modelling on the outcomes of interest    
-    statistics <- lapply(settings, function(analysis.settings) {
-        variables.to.drop <- analysis.settings$variables.to.drop
-        print(variables.to.drop)
-        print(grepl(paste0("\\b", variables.to.drop, "\\b", collapse="|"), names(study.sample)))
-        print(names(study.sample))
-        outcome.variable.name <- analysis.settings$outcome.label
-        study.sample=study.sample[, !grepl(paste0("\\b", variables.to.drop, "\\b", collapse="|"),
-                                           names(study.sample), fixed=TRUE, names(study.sample))]
-        print(str(study.sample))
+    s <- settings$s30d
+    statistics <- lapply(settings, function(s) {
+        variables.to.drop <- s$variables.to.drop
+        outcome.variable.name <- s$outcome.label
+        study.sample=study.sample[
+          , !grepl(paste0(variables.to.drop, collapse="|"), names(study.sample))
+        ]
         return (RunModelling(study.sample, outcome.variable.name))
     })
     ## Summarize the results
