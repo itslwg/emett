2dcfeae4cc4c58f2dcf09624d24001f308df7ba9
Ludde <ludviggerdin@gmail.com>
2020-08-02 21:04:03 +0200

    Save breaks

--- R/PartitionTrainAndPredict.R
+++ R/PartitionTrainAndPredict.R
@@ -3,9 +3,10 @@
 #' Partitions the study sample, fits the model and makes predictions with SuperLearner.
 #' @param study.sample Data frame. The study.sample. No default.
 #' @param outcome.variable.name Character vector of length 1. The name of the outcome variable of interest. Defaults to "s30d".
-#' @param n.partitions Numeric vector of length 1. The number of partitions to create with PartitionSample. Accepted values are 2 or 3. If 2, a train and test set is created. If 3, train, validation, and test sets are created - the models is fitted on the training set, optimal breaks is gridsearched on the validation set, and the model is tested on the test set. Defaults to 2. 
 #' @param models.names Character vector. The model names to stack in SuperLearner. Defaults to c("SL.gam", "SL.randomForest", "SL.nnet, SL.xgboost", "SL.svm")
-#' @param save.to.results Logical. If TRUE SuperLearner predictions, outcome and tc in each partition is saved to the Results list. Defaults to TRUE.
+#' @param n.partitions Numeric vector of length 1. The number of partitions to create with PartitionSample. Accepted values are 2 or 3. If 2, a train and test set is created. If 3, train, validation, and test sets are created - the models is fitted on the training set, optimal breaks is gridsearched on the validation set, and the model is tested on the test set. Defaults to 2. 
+#' @param save.sample.predictions Logical. If TRUE SuperLearner predictions, outcome and tc in each partition is saved to the results list. Defaults to TRUE.
+#' @param boot.sample Logical vector of length 1. If TRUE run is treated as a bootstrap sample, meaning e.g. thatthe SuperLearner object is not saved to disk. Defaults to FALSE 
 #' @param verbose Logical. If TRUE the modelling process is printed to console. Defaults to FALSE.
 #' @param return.samples Logical vector of length 1. If TRUE the list of samples partitioned from the study.sample is returned. Defaults to TRUE.
 #' @param use.fitted.sl Logical vector of length 1. If TRUE the file. Default/No default. 
@@ -14,8 +15,8 @@ PartitionTrainAndPredict <- function(study.sample,
                                      outcome.variable.name = "s30d",
                                      model.names = c("SL.randomForest"),
                                      n.partitions = 2,
-                                     save.to.results = TRUE,
-                                     boot = FALSE,
+                                     save.sample.predictions = TRUE,
+                                     boot.sample = FALSE,
                                      verbose = FALSE,
                                      return.samples = TRUE,
                                      use.fitted.sl = FALSE){
@@ -29,12 +30,13 @@ PartitionTrainAndPredict <- function(study.sample,
     partitions <- PartitionSample(study.sample = study.sample,
                                   outcome.variable.name = outcome.variable.name,
                                   n.partitions = n.partitions)
+    
     fitted.sl <- ""
     sl.object.file <- paste0("SuperLearner_", outcome.variable.name, ".rds")
     ## Fit the model to the training data
-    if (use.fitted.sl==TRUE) {
+    if (use.fitted.sl) {
         if (file.exists(sl.object.file)) {
-            message(paste0("Using ", sl.object.file, "..."))
+            message(paste0("Argument use.fitted.sl is TRUE and SuperLearner_", outcome.variable.name, " exists.", " Using ", sl.object.file, "..."))
             fitted.sl <- readRDS(sl.object.file)
         } else {
             if (verbose) {
@@ -54,11 +56,11 @@ PartitionTrainAndPredict <- function(study.sample,
                                                                  SL.library = model.names,
                                                                  method = "method.AUC",
                                                                  verbose = FALSE))
-    }
-    if (!boot && !use.fitted.sl) {
-        saveRDS(fitted.sl, file = sl.object.file)
-        if (verbose)
-            message(paste0("SuperLearner object saved to disk as ", sl.object.file, "..."))
+        if (!boot.sample) {
+            saveRDS(fitted.sl, file = sl.object.file)
+            if (verbose)
+                message(paste0("SuperLearner object saved to disk as ", sl.object.file, "..."))
+        }
     }
     ## Extract training sets
     train.validation <- partitions[-grep("test", names(partitions))]
@@ -69,30 +71,53 @@ PartitionTrainAndPredict <- function(study.sample,
                                                             newdata = partition.list$x,
                                                             onlySL = TRUE)$pred)
     label <- ifelse(n.partitions == 2, "train", "validation")
-    if (verbose)
-        message(paste("Finding optimal breaks for continuous probabilities on the", label, "set..."))
     ## Gridsearch the optimal cut-points for the predicted probabilities on
     ## the appropriate set
-    optimal.breaks <- GridsearchBreaks(predictions = predictions[grepl(label, con.list.labels)][[1]], 
-                                       outcome.vector = partitions[[label]]$y)
+    optimal.breaks <- ""
+    if (use.fitted.sl) {
+        results.breaks <- readRDS("results.Rds")$optimal.breaks
+        if (is.null(results.breaks)) {
+            if (verbose)
+                message(paste("Finding optimal breaks for continuous probabilities on the", label, "set..."))
+            optimal.breaks <- GridsearchBreaks(predictions = predictions[grepl(label, con.list.labels)][[1]], 
+                                               outcome.vector = partitions[[label]]$y)
+            suppressMessages({
+                bengaltiger::SaveToResults(optimal.breaks, "optimal.breaks")
+            })
+        } else {
+            message("Parameter use.fitted.sl is set to True, and results file contain optimal.breaks element. Using those as breaks for binning continous predictions...")
+            optimal.breaks <- results.breaks
+        }  
+    }
+    if (boot.sample) {
+        if (verbose)
+            message(paste("Finding optimal breaks for continuous probabilities on the", label, "set..."))
+        optimal.breaks <- GridsearchBreaks(predictions = predictions[grepl(label, con.list.labels)][[1]], 
+                                           outcome.vector = partitions[[label]]$y)   
+    }
     full.training.list <- list(y = unlist(lapply(train.validation, "[[", "y")),
                                x = do.call(rbind, lapply(train.validation, "[[", "x")))
     if (n.partitions == 3)
         ## Train the model once again. Now on both the training and validation sets
-        fitted.sl <- with(full.training.list, SuperLearner::SuperLearner(Y = y, X = x, family = binomial(),
+        fitted.sl <- with(full.training.list, SuperLearner::SuperLearner(Y = y, X = x[, c(1,2)],
+                                                                         family = binomial(),
                                                                          SL.library = model.names,
                                                                          method = "method.AUC",
-                                                                         verbose = FALSE))
+                                                                         verbose = TRUE))
     ## Make predictions on the test set
     predictions$con.model.test <- predict(object = fitted.sl,
                                           newdata = partitions$test$x,
                                           onlySL = TRUE)$pred
     ## Bin predictions made on the test set using the optimal cut-points
     cut.list.labels <- paste0("cut.model.", c("train", "validation", "test"))
-    binned.predictions <- lapply(setNames(predictions, nm = cut.list.labels),
-                                 function (preds) as.numeric(cut(x = preds, breaks = c(-Inf, optimal.breaks, Inf),
-                                                                 labels = c("Green", "Yellow", "Orange", "Red"),
-                                                                 include.lowest = TRUE)))
+    binned.predictions <- lapply(setNames(predictions, nm = cut.list.labels), function (preds) {
+        as.numeric(
+            cut(x = preds,
+                breaks = c(-Inf, optimal.breaks, Inf),
+                labels = c("Green", "Yellow", "Orange", "Red"),
+                include.lowest = TRUE)
+        )  
+    })
     NewLabelsAndNumeric <- function(label) {
         new.labels <- paste0(label, ".", names(partitions))
         new.list <- lapply(setNames(partitions, nm = new.labels),
@@ -106,9 +131,11 @@ PartitionTrainAndPredict <- function(study.sample,
         return.object$samples <- lapply(partitions, "[[", "x")
     }
     ## Save the predictions, outcome and clinicians tc in each partition to the results list
-    if (save.to.results) {
+    if (save.sample.predictions) {
         for (i in seq_along(return.object))
-            bengaltiger::SaveToResults(return.object[[i]], names(return.object)[i])
+            suppressMessages({
+                bengaltiger::SaveToResults(return.object[[i]], names(return.object)[i])  
+            })
     }
     return (return.object)
 }
--- R/PrepareSample.R
+++ R/PrepareSample.R
@@ -11,10 +11,17 @@ PrepareSample<- function(study.variables) {
     study.sample <- PrepareStudySample(study.sample = study.sample[, study.variables],
                                        data.dictionary = data.dictionary)
     ## Create bengaltiger study sample (Not included the correct inclusion.criteria functions)
-    inclusion.criteria <- c(OnlyInformedConsent, bengaltiger::OnlyAdults, OnlyPatientsWithCompleteOutcome)
-    study.sample <- bengaltiger::CreateStudySample(study.data = study.sample, complete.cases = FALSE,
-                                                   relevant.variables = names(study.sample),
-                                                   inclusion.criteria = inclusion.criteria)
+    inclusion.criteria <- c(
+        OnlyInformedConsent,
+        bengaltiger::OnlyAdults,
+        OnlyPatientsWithCompleteOutcome,
+        OnlyCompleteTriageCategory
+    )
+    suppressMessages({
+        study.sample <- bengaltiger::CreateStudySample(study.data = study.sample, complete.cases = FALSE,
+                                                       relevant.variables = names(study.sample),
+                                                       inclusion.criteria = inclusion.criteria)  
+    })
     flowchart <- bengaltiger::CreateFlowchart(compile.flowchart=TRUE, print.tikz=FALSE,
                                               intern=TRUE)
     ## Prepare the study sample, i.e. redefine levels of factors using the data dictionary and set missing
