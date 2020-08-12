3d78b30ab91094b6ed8a70f7e11993ad1efaafb1
Ludde <ludviggerdin@gmail.com>
2020-08-12 07:41:01 +0200

    Update SummarizeResults

--- R/SummarizeResults.R
+++ R/SummarizeResults.R
@@ -4,21 +4,34 @@
 #' @param modelling.list List. Output from the emett::RunModelling function. No default
 #' @export
 SummarizeResults<- function(modelling.list) {
-    ## Error handling
-    ## Create ROC-plots
-    for (nm in names(modelling.list))
-        CreateRocPlot(modelling.list[[nm]]$predictions.list, device = "pdf")
-    ## Create Mortality plot
-    CreateMortalityPlot(results$predictions.list, device = "pdf")
+    results <- readRDS("results.Rds")
+    settings <- list(
+        s30d.results=list(
+            triage.comparison.labels=c("Survived", "Died")
+        ),
+        composite.results=list(
+            triage.comparison.labels=c("Non-event", "Event")
+        )
+    )
+    for (nm in names(modelling.list)) {
+        CreateRocPlot(modelling.list[[nm]]$predictions.list,
+                      file.name=paste0("roc.plot.", sub("\\..*", "", nm)),
+                      device = "pdf")
+        CreateTriageComparisonPlot(modelling.list[[nm]]$predictions.list,
+                                   file.name=paste0("triage.comparison.plot.",  sub("\\..*", "", nm)),
+                                   pretty.labels=settings[[nm]]$triage.comparison.labels,
+                                   device = "pdf")
+    }
     ## Create table of estimates
-    estimates.with.ci <- ComputeConfidenceIntervals(boot.object = results$statistics, type = "basic")
+    estimates.with.ci <- ComputeConfidenceIntervals(boot.object = modelling.list$s30d.results$statistics,
+                                                    type = "basic")
     CreateEstimatesTable(estimates.with.ci, table.name = "estimates.table",
                          footnote = "latex", escape = FALSE, booktabs = TRUE,
                          file.format = "rmd")
     ## Create classification tables, i.e. reclassification tables
-    tables <- CreateClassificationTables(results$predictions.list)
+    tables <- CreateClassificationTables(modelling.list$s30d.results$predictions.list)
     ## Create coefficients table, i.e. measures for each individual model in SuperLearner
-    CreateCoefficientsTable(sample=results$samples$test, outcomes=results$predictions.list$y.test,
+    CreateCoefficientsTable(sample=modelling.list$samples, outcomes=results$predictions.list$y.test,
                             pretty.model.nms = pretty.names,
                             booktabs = TRUE)
     ## Create classifier plots, i.e. roc plots for all different classifiers
